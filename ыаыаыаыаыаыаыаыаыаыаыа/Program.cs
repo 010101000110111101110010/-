5ae68c9e4c73210affbdd814b24d76332214fa5e
010101000110111101110010 <maks.ogloblin.2002@mail.ru>
2020-12-18 18:35:57 +0900

    Initial commit

---
+++ ыаыаыаыаыаыаыаыаыаыаыа/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace ыаыаыаыаыаыаыаыаыаыаыа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N=m^2-2,8|m|+0,55");
+            Random rand = new Random();
+            double m = rand.Next(0, 99);
+            Console.WriteLine($"m={m}");
+            Double N = Math.Pow(m, 2) - 2.8 * Math.Abs(m) + 0.55;
+            Console.WriteLine($"N={N}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ыаыаыаыаыаыаыаыаыаыаыа/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ыаыаыаыаыаыаыаыаыаыаыа")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ыаыаыаыаыаыаыаыаыаыаыа")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fa97470e-356d-4f67-9bc6-a674666ba6de")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
